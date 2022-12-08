657b5eded6a7447a26382d46f1b99043f08eb18b
fravon <flvsadr@gmail.com>
2022-12-07 22:59:27 -0300

    listando, Inserindo, removendo

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,5 +3,10 @@ import { ProductController } from "./controllers/ProductController";
 
 const routes = Router()
 
-routes.post('/product', new ProductController().create)
+routes.post('/create', new ProductController().create)
+routes.get('/product/:id', new ProductController().find)
+routes.patch('/update/:id', new ProductController().find)
+routes.delete('/delete/:id', new ProductController().delete)
+routes.get('', new ProductController().list)
+
 export default routes
