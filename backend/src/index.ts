d88f457f4c6e8cbbd99ca19a1912032cf6a7d64b
fravon <flvsadr@gmail.com>
2022-12-14 09:52:34 -0300

    Adicionando Cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,10 +4,16 @@ import { AppDataSource } from "./data-source";
 
 import morgan from "morgan";
 
+import cors from "cors"
 import routes from "./routes";
 
 AppDataSource.initialize().then(() => {
   const app = express();
+  app.use((req, res, next)=> {
+    res.header("Access-Control-Allow-Origin", "*")
+    app.use(cors());
+    next();
+  })
   app.use(morgan("tiny"));
   app.use(express.json());
   app.use(routes);
